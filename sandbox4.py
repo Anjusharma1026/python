8e21113e54bd075e49cf4c42efa73903f8b097dc
Anjusharma1026 <anjusharmaknp34@gmail.com>
2024-08-05 15:01:21 +0530

    f

---
+++ sandbox4.py
@@ -0,0 +1,7 @@
+principle = int(input("Enter the principal:"))
+rate = int(input("Enter the rate:"))
+years = int(input("Enter the years:"))
+
+amount = (principle*rate*years)/100;
+
+print("the amount is ",amount)
